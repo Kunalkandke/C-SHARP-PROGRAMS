d35b792f4d9e01c9ec0f6da08b3c87f6810f2cb9
Kunal Kishor Kandke <163658495+Kunalkandke@users.noreply.github.com>
2026-02-08 17:31:17 +0530

    DAY-4
    Day-4
    Added 5 Programs,
    8/2/2026

---
+++ DoWhileLoopDemo.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
