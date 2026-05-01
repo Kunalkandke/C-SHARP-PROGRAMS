6a40a8dec75396e12e84d8b6e246e43dd533d439
Kunal Kishor Kandke <kunal.kandke@mit.asia>
2026-05-01 20:08:38 +0530

    DAY-35
    Implement a program to calculate the symmetric difference between two HashSet collections.

---
+++ DAY-35/program1.cs
@@ -0,0 +1,21 @@
+// Program to find the Symmetric Difference between two HashSet<T> collections
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> set1 = new HashSet<int> { 1, 2, 3, 4 };
+        HashSet<int> set2 = new HashSet<int> { 3, 4, 5, 6 };
+
+        HashSet<int> symmetricDiff = new HashSet<int>(set1);
+        symmetricDiff.SymmetricExceptWith(set2);
+
+        Console.WriteLine("Symmetric Difference:");
+        foreach (var item in symmetricDiff)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
